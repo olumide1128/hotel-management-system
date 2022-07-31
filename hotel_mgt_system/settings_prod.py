0729af5b81e3f12cbdfe720307953ffdcb92a213
Olumide Oyegoke <59411943+olumide1128@users.noreply.github.com>
2022-07-31 19:36:08 +0100

    Modified settings_prod file

--- hotel_mgt_system/settings_prod.py
+++ hotel_mgt_system/settings_prod.py
@@ -9,6 +9,6 @@ DEBUG = True
 ALLOWED_HOSTS = ['noble-hms.herokuapp.com']
 
 #postgres database for production
-db_from_env = dj_database_url.config()
+
 DATABASES = {}
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
